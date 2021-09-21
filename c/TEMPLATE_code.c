c5b6867be2d74d3605dccc240a7eceac3b8bc1f9
Kévin LE LOUËR <kevinlelo@users.noreply.github.com>
2021-09-21 15:40:54 +0200

    docs:Create code TEMPLATE in c/

---
+++ c/TEMPLATE_code.c
@@ -0,0 +1,24 @@
+/* {one line to give the file's name and a brief idea of what it does:
+ * "main.c: Defines main() for repository"}
+ *
+ * Copyright (C) 2021 Kévin LE LOUËR
+ *
+ * This file is part of starproject/{project__repository}.
+ *
+ * {repository} is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * {repository} is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with {repository}.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * To contact the author, go to
+ * <https://github.com/starproject/{project__repository}/>.
+ */
+
